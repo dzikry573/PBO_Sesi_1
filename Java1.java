b7e73293853c2a235305673a9d97c31f559ad878
dzikry573 <34776921+dzikry573@users.noreply.github.com>
2021-09-27 18:21:28 +0700

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,18 @@
+package java1;
+
+/**
+ *
+ * @author Dzikry
+ */
+public class Java1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("Belajar Java");
+     System.out.println("Sangat mudah sekali");
+     System.out.println("Dan sangat menyenangkan");
+    }
+    
+}
---
+++ Java2.java
@@ -0,0 +1,15 @@
+
+package java1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Dzikry
+ */
+public class Java2 {
+  public static void main (String[]args){
+     String Pertanyaan = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+Pertanyaan+" Sangat mudah   ");
+    }
+}
